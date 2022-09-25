10dcc4c613143f14b87e2d8fdc1123b02b432470
Tatiana Sheleh <apsua@tut.by>
2022-09-25 11:28:03 +0300

    решение задачи 50

---
+++ zadanie50/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//1 7 -> такого числа в массиве нет
+
+Console.Clear();
+
+Console.WriteLine("Введите номер строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+
+        }
+        Console.WriteLine("|");
+    }
+}
+
+bool CheckIndex(int[,] matrix, int a, int b)
+{
+    return 0 <a && a <= matrix.GetLength(0) && 0 < b && b <= matrix.GetLength(1);
+}
+
+int[,] array2D = CreateMatrixRndInt(6, 6, -99, 99);
+PrintMatrix(array2D);
+
+if (CheckIndex(array2D, a, b))
+{
+    Console.WriteLine($"{array2D[a-1, b-1]}");
+}
+else Console.WriteLine("Такого числа нет");
